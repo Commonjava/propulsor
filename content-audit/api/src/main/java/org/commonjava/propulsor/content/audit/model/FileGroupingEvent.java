f8e6d00865dd51a04ba942b0789da367f603722e
Wenjie Guo <guowj12@gmail.com>
2018-11-09 04:05:32 +0800

    Introduce the content grouping event  (#23)
    * Init content audit module
    
    * Introduce the content grouping event

---
+++ content-audit/api/src/main/java/org/commonjava/propulsor/content/audit/model/FileGroupingEvent.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/propulsor)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.propulsor.content.audit.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class FileGroupingEvent
+{
+    private UUID eventId;
+
+    private List<UUID> fileEvents;
+
+    private String nodeId;
+
+    private String eventType;
+
+    private String requestId;
+
+    private Integer eventVersion;
+
+    private Map<String, String> extra;
+
+    @JsonFormat( shape = JsonFormat.Shape.STRING )
+    private Date timestamp;
+
+    public FileGroupingEvent( String eventType )
+    {
+        this.eventId = UUID.randomUUID();
+        this.eventVersion = 1;
+        this.eventType = eventType;
+    }
+
+    public UUID getEventId() { return eventId; }
+
+    public List<UUID> getFileEvents() { return fileEvents; }
+
+    public void setFileEvents( List<UUID> fileEvents ) { this.fileEvents = fileEvents; }
+
+    public String getNodeId() { return nodeId; }
+
+    public void setNodeId( String nodeId ) { this.nodeId = nodeId; }
+
+    public String getEventType() { return eventType; }
+
+    public void setEventType( String eventType ) { this.eventType = eventType; }
+
+    public String getRequestId() { return requestId; }
+
+    public void setRequestId( String requestId ) { this.requestId = requestId; }
+
+    public Integer getEventVersion() { return eventVersion; }
+
+    public void setEventVersion( Integer eventVersion ) { this.eventVersion = eventVersion; }
+
+    public Map<String, String> getExtra() { return extra; }
+
+    public void setExtra( Map<String, String> extra ) { this.extra = extra; }
+
+    public Date getTimestamp() { return timestamp; }
+
+    public void setTimestamp( Date timestamp ) { this.timestamp = timestamp; }
+
+}

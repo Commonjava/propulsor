6f2ace58d84a7ce73ce2387bfb498630ce7ebda9
Rui Han <ruhan@redhat.com>
2019-01-16 13:05:06 +0800

    Fix license headers

--- content-audit/kafka-publisher/src/main/java/org/commonjava/propulsor/KafkaPublisher.java
+++ content-audit/kafka-publisher/src/main/java/org/commonjava/propulsor/KafkaPublisher.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/propulsor)
+ * Copyright (C) 2015 John Casey (jdcasey@commonjava.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.commonjava.propulsor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
